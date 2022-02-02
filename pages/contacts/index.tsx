140a9b6f2b1e48544c2c0cdd662ee6400b01d24b
Sergey Chistov <s.chistov@globus-ltd.com>
2022-02-02 17:56:26 +0300

    map added

---
+++ pages/contacts/index.tsx
@@ -0,0 +1,26 @@
+import styles from "./Contacts.module.scss";
+import React from "react";
+import { YMaps, Map, Placemark } from 'react-yandex-maps';
+import Navbar from "../../components-common/Navbar";
+
+const Contacts = () => {
+  return (
+    <div>
+    <Navbar/>
+  <div className={styles.container}>
+      <h2>Контакты</h2>
+      <div>ООО "АФС"</div>
+      <div className={styles.mb15}>г.Н.Новгород, ул.Федосеенко д.55</div>
+    <YMaps>
+      <div>
+        <Map width="100%" height="50vh"  defaultState={{ center: [56.338474, 43.809498], zoom: 17 }} >
+          <Placemark geometry={ [56.338474, 43.809498]}></Placemark>
+        </Map>
+      </div>
+    </YMaps>
+      </div>
+    </div>
+  )
+}
+
+export default Contacts;
