47a038635f229d1e1e6f057ef15f221def7324e4
Sergey Chistov <s.chistov@globus-ltd.com>
2022-03-14 15:02:28 +0300

    new item started

--- pages/tech/gaspardo/item/[id].tsx
+++ pages/tech/gaspardo/item/[id].tsx
@@ -129,7 +129,7 @@ export default GaspardoItem;
 export async function getStaticPaths() {
   // Call an external API endpoint to get posts
   // const res = await fetch('https://.../posts')
-  const ids = Array.from(Array(55).keys())
+  const ids = Array.from(Array(70).keys())
 
   // Get the paths we want to pre-render based on posts
   const paths = ids.map(id => ({
