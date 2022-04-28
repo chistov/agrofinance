f0855dedddfbe92f123ab2155cba2d923b59f477
Sergey Chistov <s.chistov@globus-ltd.com>
2022-04-28 14:56:45 +0300

    rm card. bugfix

--- pages/api/admin/add-card.ts
+++ pages/api/admin/add-card.ts
@@ -3,14 +3,21 @@ import middleware from '../../../middleware/middleware'
 import nextConnect from 'next-connect'
 import * as fs from "fs";
 import execQuery from "../../../db";
+import {IncomingMessage} from "http";
 
 const handler = nextConnect()
 handler.use(middleware)
 
+interface Req extends IncomingMessage {
+  body: {
+    brand: [],
+    hdr: [],
+    body: [],
+  }
+}
 
-handler.post(async (req, res) => {
-  console.log('body: ', req.body)
-  console.log('req files: ', req.files)
+
+handler.post(async (req:Req, res) => {
   const brand = req.body.brand[0];
 
   // const saveFile = (name: string, img: any) => fs.writeFileSync(`imgs/${name}`, img);
@@ -18,11 +25,9 @@ handler.post(async (req, res) => {
   console.log('ins: ', insert);
   const result = await execQuery(insert, []);
   const idx = result.insertId;
-  console.log('idx: ', idx);
 
   const filePath = req.files.picture[0].path;
   const ext = filePath.substring(filePath.lastIndexOf('.'))
-  console.log('ext: ', ext);
   const img = fs.readFileSync(filePath);
   const path = `/spare-parts/${brand}/${idx}${ext}`; // путь для img src страницы запчастей
   fs.writeFile('public' + path, img, function(err) {
@@ -32,8 +37,6 @@ handler.post(async (req, res) => {
 
   const update = `UPDATE ${brand} SET path='${path}' WHERE id=${idx}`;
   await execQuery(update, []);
-
-  //...
 })
 
 export const config = {
@@ -53,54 +56,3 @@ interface Res {
   status: (n: number) => Res;
   json: ({}) => void;
 }
-
-
-// const asyncParse = (req: any) =>
-//   new Promise((resolve, reject) => {
-//     const form = new IncomingForm({ multiples: true });
-//     console.log('form: ', form)
-//     try {
-//       form.parse(req, (err, fields, files) => {
-//         console.log('before if', err);
-//         if (err) return reject(err);
-//         console.log('resolve', files);
-//         resolve({fields, files});
-//       });
-//     }
-//     catch (e) {
-//       console.log('parse fallen: ', e);
-//     }
-//     console.log('after parse');
-//   });
-//
-//
-// export default async function handler(req: Req, res: Res) {
-//   console.log('handler: ', req);
-//
-//   if (req.method != 'POST') {
-//     console.log('post not found: ');
-//     res.status(405).json({ message: 'Only POST requests allowed' })
-//     return
-//   }
-//
-//   // console.log('boud found: ');
-//   // const body = JSON.parse(req.body);
-//   // console.log('body: ', body);
-//   // for (var [key, value] of req.entries()) {
-//   //   console.log(key, value);
-//   // }
-//   const form = new IncomingForm({ multiples: true });
-//   form.uploadDir = "./";
-//   form.keepExtensions = true;
-//
-//   form.parse(req.body, (err, fields, files) => {
-//     console.log('before if', err, fields, files);
-//   });
-//   // const result = await asyncParse(req.body)
-//   //   .then(res => {
-//   //     console.log('res: ', res)
-//   //     saveFile('1', res)
-//   //   })
-//   //   .catch(e => console.log('catch: ', e));
-//   res.status(200).json({ res: 'ok' });
-// }
---
+++ pages/api/admin/rm-card.ts
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import execQuery from "../../../db";
+import fs from "fs";
+
+type Data = {
+  name: string
+}
+
+export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  req.body = JSON.parse(req.body);
+  console.log('req: ', req.body);
+  const brand = req.body.brand;
+  const id = req.body.id;
+  let query = `select path from ${brand} where id=${id}`;
+  const r = await execQuery(query, []);
+  console.log('r: ')
+  const path = r[0].path;
+  if(path) {
+    query = `delete from ${brand} where id=${id}`;
+    await execQuery(query, []);
+    fs.unlink('public' + path, function(err) {
+      if (err) console.error(err);
+      console.log('success rm file = ', path);
+      return res.status(200).json({result: 'test'});
+    });
+  }
+  else {
+    res.status(400).json({ result: 'Error with item remove' })
+  }
+}
