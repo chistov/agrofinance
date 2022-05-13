3102aaff52b9edd59ccc49dc5f40704c7a153e95
Sergey Chistov <s.chistov@globus-ltd.com>
2022-05-13 11:29:03 +0300

    latest changes

--- components-common/helpers.ts
+++ components-common/helpers.ts
@@ -1,5 +1,15 @@
+import execQuery from "../db";
+import jwt from "jsonwebtoken";
+import {ServerResponse} from "http";
+
 export function changeMenuItem(curr: number, cb: Function)  {
-  console.log('cb: ', curr);
   cb(curr);
   window.scrollTo({top: 0});
+}
+
+export async function isAdmin(res: ServerResponse, token: string, KEY: string) {
+  // @ts-ignore
+  const [creds] = await execQuery("select * from `users`;", []);
+  const origToken = jwt.sign(creds.user, KEY);
+  return origToken == token ? true: false;
 }
--- pages/api/admin/rm-card.ts
+++ pages/api/admin/rm-card.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import execQuery from "../../../db";
 import fs from "fs";
+import {isAdmin} from "../../../components-common/helpers";
 
 type Data = {
   name: string
@@ -8,12 +9,17 @@ type Data = {
 
 export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   req.body = JSON.parse(req.body);
-  console.log('req: ', req.body);
-  const brand = req.body.brand;
-  const id = req.body.id;
+  const {token} = req.body;
+  if(!await isAdmin(res, token, process.env.JWT_KEY as string)) {
+    // @ts-ignore
+    res.status(401).json({ result: 'Недостаточно прав' })
+    res.end();
+    return;
+  }
+
+  const {brand, id} = req.body;
   let query = `select path from ${brand} where id=${id}`;
   const r = await execQuery(query, []);
-  console.log('r: ')
   // @ts-ignore
   const path = r[0].path;
   if(path) {
@@ -21,7 +27,6 @@ export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
     await execQuery(query, []);
     fs.unlink('public' + path, function(err) {
       if (err) console.error(err);
-      console.log('success rm file = ', path);
       // @ts-ignore
       return res.status(200).json({result: 'test'});
     });
