23b8c5c367ffe4684902a0f4dd567f8d30d15d5c
Sergey Chistov <s.chistov@globus-ltd.com>
2023-03-01 13:03:54 +0300

    404 page

--- pages/404/index.js
+++ pages/404/index.js
@@ -1,3 +1,13 @@
+import Link from "next/link";
+import styles from "../../components-common/404.module.scss";
+import React from "react";
+
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>
+  return <div className={styles.container}>
+    <img className={styles.img} data-lazy-type="img" src="/assets/broken.jpg" alt='404 not found'/>
+    <h2>404 - Страница не найдена</h2>
+    <Link href='/'>
+        <a className={styles.link}>Перейти на главную</a>
+    </Link>
+  </div>
 }
