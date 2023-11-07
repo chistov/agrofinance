5559de35508bd68ee48e72f7ff978166110a8c83
Sergey Chistov <s.chistov@globus-ltd.com>
2023-11-07 14:53:11 +0300

    layout base functions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { StrictMode } from 'react';
 
 function MyApp({ Component, pageProps: {...pageProps} }: AppProps) {
   const AnyComponent = Component as any;
-  return (
-      <AnyComponent {...pageProps} />
+  const getLayout = AnyComponent.getLayout || ((page) => page)
+  return getLayout(
+      <StrictMode>
+        <AnyComponent {...pageProps} />
+      </StrictMode>
   )
 }
 
